bdb7661d82e46666114dd7f290a633a7aaba3d0d
rmakula2025 <rmakula2025@gmail.com>
2025-10-02 12:13:44 -0500

    Update API base URL to Render backend

--- mini-emr-frontend/src/api/api.js
+++ mini-emr-frontend/src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000";
+// Use your Render backend URL instead of localhost
+const BASE_URL = "https://z-backend-gz51.onrender.com";
 
 // Admin API instance
 export const adminAPI = axios.create({
